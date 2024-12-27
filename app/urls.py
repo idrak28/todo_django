98c7cf97025a4158278d2927580e0511bc64c993
idrak28 <idrakefaz99@gmail.com>
2024-12-27 21:05:26 +0600

    done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TODOListView ,TODOFormView
+from .views import TODOListView ,TODOFormView ,TODOUpdateView ,TaskDelete
 urlpatterns = [
     path('', TODOListView.as_view(), name='todo_list'),
     path('form',TODOFormView.as_view(),name='todo_form'),
-  
+    
+    path('<pk>/update', TODOUpdateView.as_view(),name="todo_update"),
+    path('<pk>/delete', TaskDelete.as_view(),name="todo_delete"),
 ] 
--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from django.views.generic import ListView
 from .forms import TODOForm
 from django.contrib.auth.decorators import login_required  #login
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView ,UpdateView ,DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth import login 
+
+from django.contrib import messages 
 class TODOListView(ListView):
     model = TODO
     template_name='todo_list.html'
@@ -28,5 +30,24 @@ class TODOFormView(FormView):
         todo.save()
         return super(TODOFormView,self).form_valid(form)
     
-        
     
+class TODOUpdateView(UpdateView):
+    model=TODO
+    template_name = "todo_update.html"
+    #queryset =TODO.objects.datetimes()
+    fields =[
+        "text",
+        "deadline_at" ,
+    ]        
+    success_url ="/"
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TODOUpdateView,self).form_valid(form)
+class TaskDelete(DeleteView):
+    model = TODO
+    template_name = "todo_delete.html"
+    success_url ='/'
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(TaskDelete,self).form_valid(form)
